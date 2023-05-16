46f08f65200e6d665836317b6e6adb387a957146
endorphinchik <elisaveta.stepura@mail.ru>
2023-05-16 19:33:31 +0300

    new

--- src/main/java/com/example/servingwebcontent/EventsController.java
+++ src/main/java/com/example/servingwebcontent/EventsController.java
@@ -1,10 +1,10 @@
 package com.example.servingwebcontent;
 
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -34,25 +34,58 @@ class Event
 @Controller
 public class EventsController {
     static final ArrayList<Event> events = new ArrayList<Event>(){{
+        add(new Event("Opera", "London"));
         add(new Event("Violin concert", "Prague"));
-        add(new Event("Jazz concert", "Vienna"));
-        add(new Event("Art exhibition", "Munich"));
-        add(new Event("Art-house exhibition", "Amsterdam"));
-        add(new Event("Requiem Verdi concert", "Berlin"));
+        add(new Event("Jazz concert", "Berlin"));
+        add(new Event("Art exhibition", "London"));
     }};
 
-    @RequestMapping(value = "/events", method = RequestMethod.GET)
-    public String listEventsChangeName(@RequestParam(name="city", required = false, defaultValue = "all") String city, Model model)
-    {
-        List<Event>result = events;
-        if(!city.equals("all")){
-            result= events.stream().filter(e -> e.getCity().equals(city)).collect(Collectors.toList());
+    @Operation(summary = "Get all events", description = "Get all events filtered by city")
+    @RequestMapping(value = "/events", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<Event> listEventsChangeName(@RequestParam(name = "city", required = false, defaultValue = "all") String city, Model model) {
+        List<Event> result = events;
+        if (!city.equals("all")) {
+            result = events.stream().filter(e -> e.getCity().equals(city)).collect(Collectors.toList());
         }
-        model.addAttribute("events", result);
-//        List<Event> cityEvents = events.stream().filter(e -> e.getCity().equals(city)).collect(Collectors.toList());
-//        model.addAttribute("events", cityEvents); // Pass data to view
-        return "eventsview"; // Return name of view
+        return result;
     }
+
+    @Operation(summary = "Get  events", description = "Get specify event by id")
+    @GetMapping(value = "/events/{id}")
+    @ResponseBody
+    public Event getEvent(@PathVariable int id)
+    {
+        Event event = events.get(id);
+        return event;
+    }
+
+    @Operation(summary = "Post  events", description = "Create event")
+    @PostMapping(value = "/events")
+    @ResponseBody
+    public Event createEvent(@RequestBody Event event)
+    {
+        // TODO save to database
+        return event;
+    }
+    @Operation(summary = "Delete  events", description = "Delete event by id")
+    @DeleteMapping(value = "/events/{id}")
+    @ResponseBody
+    public Event deleteEvent(@PathVariable int id)
+    {
+        Event event = events.get(id);
+        // TODO: remove it from the database
+        return event;
+    }
+
+    @PutMapping(value = "/events/{id}")
+    @ResponseBody
+    public Event updateEvent(@PathVariable int id, @RequestBody Event event)
+    {
+        Event origEvent = events.get(id);
+        // TODO: update event in the database
+        origEvent = event; // useless just for example
+        return event;
+    }
+
 }
-
-
--- src/main/java/com/example/servingwebcontent/GreetingController.java
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -10,10 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-//    @GetMapping("/greeting")
-    @RequestMapping(value = "/greeting",method = RequestMethod.GET)
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    //@GetMapping("/greeting")
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
